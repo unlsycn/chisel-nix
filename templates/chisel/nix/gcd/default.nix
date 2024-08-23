0a09c074b448a8579f2387b29684051ebdfe7c26
unlsycn <unlsycn@unlsycn.com>
2024-08-24 03:21:46 +0800

    [nix] update nix scripts
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- templates/chisel/nix/gcd/default.nix
+++ templates/chisel/nix/gcd/default.nix
@@ -1,16 +1,34 @@
-{ lib
-, newScope
-,
-}:
+{ lib, newScope, }:
 lib.makeScope newScope (scope: {
-  gcd-compiled = scope.callPackage ./gcd.nix { };
-  inherit (scope.gcd-compiled) elaborator;
+  design-target = "GCD";
+  tb-target = "GCDTestBench";
 
-  elaborate = scope.callPackage ./elaborate.nix { };
+  # RTL
+  gcd-compiled = scope.callPackage ./gcd.nix { target = scope.design-target; };
+  elaborate = scope.callPackage ./elaborate.nix {
+    elaborator = scope.gcd-compiled.elaborator;
+  };
   mlirbc = scope.callPackage ./mlirbc.nix { };
   rtl = scope.callPackage ./rtl.nix { };
 
-  verilated-c-lib = scope.callPackage ./verilated-c-lib.nix { };
+  tb-compiled = scope.callPackage ./gcd.nix { target = scope.tb-target; };
+  tb-elaborate = scope.callPackage ./elaborate.nix {
+    elaborator = scope.tb-compiled.elaborator;
+  };
+  tb-mlirbc =
+    scope.callPackage ./mlirbc.nix { elaborate = scope.tb-elaborate; };
+  tb-rtl = scope.callPackage ./rtl.nix { mlirbc = scope.tb-mlirbc; };
+  tb-dpi-lib = scope.callPackage ./dpi-lib.nix { };
+  tb-dpi-lib-trace = scope.tb-dpi-lib.override { enable-trace = true; };
+
+  verilated = scope.callPackage ./verilated.nix { rtl = scope.tb-rtl; };
+  verilated-trace =
+    scope.verilated.override { tb-dpi-lib = scope.tb-dpi-lib-trace; };
   vcs = scope.callPackage ./vcs.nix { };
+
+  # TODO: designConfig should be read from OM
+  tbConfig = with builtins;
+    fromJSON (readFile ./../../configs/${scope.tb-target}.json);
+
 })
 
